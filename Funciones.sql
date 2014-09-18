52882b295e6e87e55bd9b9e3ec5faf3b0fee570d
Emas87 <emanu.87@gmail.com>
2014-09-17 18:21:45 -0600

    Funciones Listas
    EOM

--- Funciones.sql
+++ Funciones.sql
@@ -118,47 +118,54 @@ END Consulta_L_Prestados;
 --exec :LibrosP := Consulta_L_Prestados('P');
 --print LibrosP;
 
+CREATE or REPLACE Function Consulta_Top_N --Muestra el top N de libros prestados 
+RETURN types.cursorType
+AS 
+Rcursor types.cursorType;
+BEGIN
+	OPEN Rcursor FOR
+	SELECT IDGENERAL
+	FROM (	SELECT IDGENERAL,COUNT(IDGENERAL) 
+			FROM ARTPREST
+			GROUP BY IDGENERAL
+			ORDER BY COUNT(IDGENERAL) DESC)
+	WHERE ROWNUM <= (SELECT VALOR FROM PARAMETROS WHERE NOMPARAM = 'TOP');
+	--WHERE Ocupado = 1 AND 
+	--TipArt = (SELECT Codigo FROM CatTipoArt WHERE TipArt = 'Libro') AND 
+	--IDGENERAL = (select IDGENERAL FROM ArtPrest WHERE IDPersona = (SELECT IDPERSONA FROM Persona WHERE Nombre = NombreP))
+	--ORDER BY TITULO;
+	RETURN Rcursor;
+END Consulta_Top_N;
+
+--El test de la funcion anterior
+--var LibrosP refcursor;
+--exec :LibrosP := Consulta_Top_N();
+--print LibrosP;
+
+CREATE or REPLACE Function Consulta_P_n_veces_en_m --Muestra los libros prestados n veces en los ultimos m meses 
+RETURN types.cursorType
+AS 
+Rcursor types.cursorType;
+temp int;
+tempd date;
+BEGIN
+  SELECT VALOR INTO temp FROM PARAMETROS WHERE NOMPARAM = 'M'; --obtener parametro
+  DBMS_OUTPUT.PUT_LINE('Parametro '|| temp);
+  tempd := ADD_MONTHS(TO_DATE(TRUNC(sysdate),'dd/mm/yyyy'),-1*temp);
+	OPEN Rcursor FOR
+      SELECT IDGENERAL 
+			FROM ARTPREST
+			WHERE TO_DATE(TRUNC(FECHAPRESTAMO),'dd/mm/yyyy') > ADD_MONTHS(TO_DATE(TRUNC(sysdate),'dd/mm/yyyy'),-1*temp)
+			GROUP BY IDGENERAL
+			HAVING COUNT(IDGENERAL) >= (SELECT VALOR FROM PARAMETROS WHERE NOMPARAM = 'N')
+			ORDER BY COUNT(IDGENERAL) DESC;
+--	WHERE COUNT(IDGENERAL) >= (SELECT VALOR FROM PARAMETROS WHERE NOMPARAM = 'N');
+	RETURN Rcursor;
+END Consulta_P_n_veces_en_m;
+
+--El test de la funcion anterior
+--var LibrosP refcursor;
+--exec :LibrosP := Consulta_P_n_veces_en_m();
+--print LibrosP;
 
 --#############################################################################################################
-
-CREATE or REPLACE PROCEDURE Consulta_L_N_Prestados()
-AS 
-BEGIN
-	SELECT *
-	FROM Articulo
-	ORDER BY TITULO
-	WHERE Ocupado = 1 AND TipArt = (SELECT Codigo FROM CatTipoArt WHERE TopArt = "Libro");
-END;
-
-CREATE or REPLACE PROCEDURE Consulta_top_n()
-AS 
-BEGIN
-	CREATE VIEW NumPrest As
-		SELECT ID_GENERAL,COUNT(ID_GENERAL)
-		FROM ArtPrest;
-	SELECT *
-	FROM (SELECT * FROM NumPrest ORDER BY COUNT(ID_GENERAL)
-	WHERE ROWNUM <= (SELECT VALOR FROM Parametro WHERE NomParam = "Top");
-	DROP VIEW NumPrest;
-END;
-
-SELECT *
-FROM (SELECT * FROM EMPLOYEE ORDER BY SALARIO DESC)
-WHERE ROWNUM <= 3;
-
-
-CREATE or REPLACE PROCEDURE Consulta_P_n_veces_en_m()
-AS 
-BEGIN
-	CREATE VIEW NumPrest As
-		SELECT ID_GENERAL,COUNT(ID_GENERAL)
-		FROM ArtPrest;
-	SELECT *
-	FROM (SELECT * FROM NumPrest ORDER BY COUNT(ID_GENERAL)
-	WHERE ROWNUM <= (SELECT VALOR FROM Parametro WHERE NomParam = "Top");
-	DROP VIEW NumPrest;
-END;
-
-SELECT *
-FROM (SELECT * FROM EMPLOYEE ORDER BY SALARIO DESC)
-WHERE ROWNUM <= 3;
